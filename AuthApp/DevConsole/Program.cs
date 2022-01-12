82968806b70c3c8687bd17e2f9c4689d008661d6
carlandres18@gmail.com <carlandres18@gmail.com>
2022-01-12 19:45:57 +0800

    Appsettings

---
+++ AuthApp/DevConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Access;
+using Data.Sql.Provider;
+
+Console.WriteLine("Hello, World!");
+
+static void InitializeUserAccess()
+{
+    SqlServerProvider sqlServerProvider = new SqlServerProvider(@"Data Source='DESKTOP-U64U4KB\SQLEXPRESS';Initial Catalog=UserAccessManagementDb;User ID=sa;Password=password;");
+
+    UserAccess userAccess = UserAccess.New("qazzqc@foo.company", )
+}
